62a75447578bc1fc82598caa769e0e49480b3c5b
Christian Bager Bach Houmann <chhoumann@gmail.com>
2021-06-18 00:15:59 +0200

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const path = require('path')
+require('dotenv').config();
+
+const middlewares = require('./middlewares');
+
+const app = express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/index.html'))
+});
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+const port = process.env.PORT || 2020;
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+});
