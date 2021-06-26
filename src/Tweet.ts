546394faf503311fe5de7a7703cdff8dd1aec58a
Christian Bager Bach Houmann <chhoumann@gmail.com>
2021-06-26 04:14:35 +0200

    Initial setup

--- src/Tweet.ts
+++ src/Tweet.ts
@@ -3,9 +3,12 @@ import {ITweet} from "./ITweet";
 export class Tweet implements ITweet {
     id: string;
     content: string[];
+    postat: Date;
 
-    constructor(id: string, tweet: string[]) {
+    constructor(id: string, tweet: string[], postat: Date) {
         this.id = id;
         this.content = tweet;
+        this.postat = postat;
     }
+
 }
