fabc3d2243557a4dff25ceb993afb4a6d7148bea
Christian Bager Bach Houmann <chhoumann@gmail.com>
2021-06-26 02:53:20 +0200

    Initial setup

--- src/tweetStore.ts
+++ src/tweetStore.ts
@@ -3,24 +3,24 @@ import {ITweet} from "./ITweet";
 const db = require('./db');
 
 export class TweetStore {
-    public init(): void {
-        db.query("CREATE TABLE tweets (id text primary key, content text[]);");
+    public async init(): Promise<void> {
+        await db.query("CREATE TABLE tweets (id text primary key, content text[]);");
     }
 
-    public getTweets(): ITweet[] {
-        return db.query("SELECT * from public.tweets;");
+    public async getTweets(): Promise<ITweet[]> {
+        return await db.query("SELECT * from public.tweets;");
     }
 
-    public addTweet(tweet: ITweet): void {
-        db.query(`INSERT INTO public.tweets (id, content) values (${tweet.id}, {${tweet.content.join(', ')}});`);
+    public async addTweet(tweet: ITweet): Promise<void> {
+        await db.query(`INSERT INTO public.tweets(id, content) values ($1, $2);`, [tweet.id, tweet.content]);
     }
 
-    public deleteTweet(tweetId: string): void {
-        db.query(`DELETE FROM public.tweets WHERE id = ${tweetId}`);
+    public async deleteTweet(tweetId: string): Promise<void> {
+        await db.query(`DELETE FROM public.tweets WHERE id = ${tweetId}`);
     }
 
-    public updateTweet(tweetId: string, newData: ITweet): ITweet {
-        db.query(`UPDATE public.tweets SET content = ${newData.content.join(', ')} WHERE id = ${tweetId};`)
+    public async updateTweet(tweetId: string, newData: ITweet): Promise<ITweet> {
+        await db.query(`UPDATE public.tweets SET content = ${newData.content.join(', ')} WHERE id = ${tweetId};`)
         return newData;
     }
 }
