587f05af28474f8c9644405d748412f8f495445d
Christian Bager Bach Houmann <chhoumann@gmail.com>
2021-07-01 10:11:32 +0200

    Update tweetStore.ts

--- src/tweetStore.ts
+++ src/tweetStore.ts
@@ -4,7 +4,7 @@ const db = require('./db');
 
 export class TweetStore {
     public async init(): Promise<void> {
-        await db.query("CREATE TABLE tweets(id text primary key, content text[], postOn timestamp);");
+        await db.query("CREATE TABLE tweets(id text primary key, content text[], postat timestamp);");
     }
 
     public async getTweets(): Promise<ITweet[]> {
@@ -22,7 +22,7 @@ export class TweetStore {
     public async updateTweet(tweetId: string, newData: ITweet): Promise<ITweet> {
         console.log("Trying to update.", newData);
         
-        await db.query(`UPDATE public.tweets SET content = $1, poston = $2 WHERE id = $3;`, [newData.content, newData.postat, tweetId])
+        await db.query(`UPDATE public.tweets SET content = $1, postat = $2 WHERE id = $3;`, [newData.content, newData.postat, tweetId])
         return newData;
     }
 }
